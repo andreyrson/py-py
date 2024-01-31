7d35750b9bcf075d4b452c7ad185684caf373710
andrey de matos <matosandrey13@gmail.com>
2024-01-31 11:41:30 -0300

    pŕimeiro commit

---
+++ two.py
@@ -0,0 +1,12 @@
+notas = []
+
+for x in range (5):
+    codigo_aluno = input('RM: ')
+    nota = float(input('NOTA: '))
+    resultado = [codigo_aluno , nota]
+    notas.append(resultado)
+    print('quantidade de notas' , len(notas))
+    for n in notas:
+        codigo_aluno = n[0]
+        nota = n[1]
+        print('o RM' , codigo_aluno , 'tirou a nota', nota)
